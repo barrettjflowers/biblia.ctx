9db890bf8202c1c7d1356dac74c9436f52ec458b
Barrett Flowers <barrettjflowers@gmail.com>
2026-01-28 21:43:19 -0500

    first commit, added types for the dict

---
+++ src/lib/dict.ts
@@ -0,0 +1,12 @@
+import type { Cononical } from './types'
+
+export const insight: Cononical[] = [
+  {
+    id: 'moabite-stone',
+    title: 'The Moabite Stone',
+    description: '9th-century BCE inscription by King Mesha',
+    date: 'c. 840 BCE', //this should probably be an integer so we can sort by other nearby ids
+    tags: ['inscription', 'moabite']
+  }
+]
+
---
+++ src/lib/types.ts
@@ -0,0 +1,16 @@
+export type Cononical = {
+  id: string
+  title: string
+  description: string
+  date?: string
+  tags?: string[]
+}
+
+export type Apocryphal = {
+  id: string
+  title: string
+  description: string
+  date?: string
+  tags?: string[]
+}
+
