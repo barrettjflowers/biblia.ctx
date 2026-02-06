029639482229ca758591ab0cb2d990f721a9e0c9
Barrett Flowers <barrettjflowers@gmail.com>
2026-02-05 21:09:21 -0500

    add insight book dict

--- src/lib/dict.ts
+++ src/lib/dict.ts
@@ -2,11 +2,11 @@ import type { Cononical } from './types'
 
 export const insight: Cononical[] = [
   {
-    id: 'moabite-stone',
-    title: 'The Moabite Stone',
-    description: '9th-century BCE inscription by King Mesha',
-    date: 'c. 840 BCE', //this should probably be an integer so we can sort by other nearby ids
-    tags: ['inscription', 'moabite']
+    id: 'moabite-stone', //db key
+    title: 'The Moabite Stone', //userspace friendly name
+    description: '9th-century BCE inscription by King Mesha', //userspace description
+    date: 'c. 840 BCE', //date
+    tags: ['inscription', 'moabite'] //search keys
   }
 ]
 
